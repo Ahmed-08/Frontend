9ba2fd45665061097b4fb1d0ebd46ce8a97c8b95
Ahmed-08 <58491091+Ahmed-08@users.noreply.github.com>
2025-01-21 16:08:44 +0300

    Delete github/createx-ts directory

--- github/createx-ts/src/shared/components/index.ts
+++
@@ -1,5 +0,0 @@
-export {Button} from './button/Button.tsx';
-export {Cart} from './cart/Cart.tsx';
-export {EventBlock} from './eventblock/EventBlock.tsx';
-export {Pagination} from './pagination/Pagination.tsx';
-export {EventFilter} from '../../pages/Events/ui/eventfilter/EventFilter.tsx';
--- github/createx-ts/src/widgets/index.ts
+++
@@ -1,4 +0,0 @@
-export {Login} from './access/login/Login.tsx';
-export {Header} from './header/Header.tsx';
-export {Footer} from './footer/Footer.tsx';
-export {Register} from './access/register/Register.tsx';
